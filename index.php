044c325defc6fd0e659079b2854bfcaf5e83cd38
Calopsia <pseba20@gmail.com>
2017-01-30 17:46:03 -0300

    ..

--- index.php
+++ index.php
@@ -9,4 +9,18 @@ $app->get('/hello/{name}', function ($request, $response, $args) {
     return $response;
 });
 
+
+$app->post('/upload', function ($request, $response, $args) {
+    $files = $request->getUploadedFiles();
+    if (empty($files['newfile'])) {
+        throw new Exception('Expected a newfile');
+    }
+
+    $newfile = $files['newfile'];
+    if ($newfile->getError() === UPLOAD_ERR_OK) {
+        $uploadFileName = $newfile->getClientFilename();
+        $newfile->moveTo("uploads/$uploadFileName");
+    }
+});
+
 $app->run();
